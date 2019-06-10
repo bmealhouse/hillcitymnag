5795696840464b65ddf1885c415a6d258e405efd
Brent Mealhouse <bmealhouse@gmail.com>
2019-06-09 21:14:04 -0500

    Add logging & disable prefetch-data on pre-commit

--- api/deploy/src/deploy.js
+++ api/deploy/src/deploy.js
@@ -10,19 +10,19 @@ const DEPLOY_DIR = path.resolve('/tmp')
 module.exports = async (owner, repo) => {
   const url = `https://github.com/${owner}/${repo}/archive/master.tar.gz`
 
-  // console.log(`Downloading ${url}`)
+  console.log(`Downloading ${url}`)
   await download(url, DEPLOY_DIR, {extract: true})
 
   const cwd = path.join(DEPLOY_DIR, `${repo}-master`)
   const zeitToken = process.env.HCAG_ZEIT_TOKEN
 
   try {
-    // console.log(`Deploying ${owner}/${repo}`)
+    console.log('Prefetch data')
     await prefetchData(cwd)
+
+    console.log(`Deploying ${owner}/${repo}`)
     await now(zeitToken, cwd)
-    // const url = await now(zeitToken, cwd)
-    // await verifyDeployment(url)
-    // await aliasDeployment(zeitToken, cwd)
+
     Raven.captureMessage(`Deployed ${owner}/${repo}`, {level: 'info'})
   } finally {
     await rimraf(cwd)
--- api/deploy/src/utils.js
+++ api/deploy/src/utils.js
@@ -5,13 +5,8 @@ const exec = promisify(require('child_process').exec)
 const fetch = require('node-fetch')
 
 const NOW = path.resolve('./node_modules/.bin/now')
-const TIMEOUT = process.env.NODE_ENV === 'test' ? 0 : 30000
 
 module.exports = {
-  async aliasDeployment(zeitToken, cwd) {
-    // console.log('Aliasing for production')
-    await exec(`${NOW} alias --token ${zeitToken}`, {cwd})
-  },
   checksum(data) {
     const text = JSON.stringify(data)
     const hash = crypto.createHash('sha1')
@@ -33,30 +28,5 @@ module.exports = {
       {cwd},
     )
     return stdout
-  },
-  verifyDeployment(url) {
-    return new Promise((resolve, reject) => {
-      let attempt = 1
-
-      async function verify() {
-        // console.log('  ...attempting to verify deployment')
-
-        if (attempt > 10) {
-          return reject(new Error('Could not verify deployment'))
-        }
-
-        const res = await fetch(url)
-        const html = await res.text()
-
-        if (html.includes('Hill City Assembly of God Church')) {
-          return resolve()
-        }
-
-        attempt += 1
-        setTimeout(verify, TIMEOUT)
-      }
-
-      verify()
-    })
   },
 }
