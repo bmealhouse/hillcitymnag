9bc4d906f24fc96355ad4136f631b1539d0ff81f
Brent Mealhouse <bmealhouse@gmail.com>
2019-06-09 21:26:34 -0500

    Adjust logging and capture swallowed exceptions

--- api/deploy/src/deploy.js
+++ api/deploy/src/deploy.js
@@ -10,20 +10,22 @@ const DEPLOY_DIR = path.resolve('/tmp')
 module.exports = async (owner, repo) => {
   const url = `https://github.com/${owner}/${repo}/archive/master.tar.gz`
 
-  console.log(`Downloading ${url}`)
+  // console.log(`Downloading ${url}`)
   await download(url, DEPLOY_DIR, {extract: true})
 
   const cwd = path.join(DEPLOY_DIR, `${repo}-master`)
   const zeitToken = process.env.HCAG_ZEIT_TOKEN
 
   try {
-    console.log('Prefetch data')
+    // console.log(`Deploying ${owner}/${repo}`)
     await prefetchData(cwd)
-
-    console.log(`Deploying ${owner}/${repo}`)
     await now(zeitToken, cwd)
 
     Raven.captureMessage(`Deployed ${owner}/${repo}`, {level: 'info'})
+  } catch (error) {
+    console.log(error)
+    Raven.captureException(error)
+    throw error
   } finally {
     await rimraf(cwd)
   }
