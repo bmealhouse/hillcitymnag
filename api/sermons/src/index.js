a3cbec4651b8351ca39650e9aa134a2f147b26b0
Brent Mealhouse <bmealhouse@gmail.com>
2020-02-08 10:21:53 -0600

    Remove api/sermons

--- api/sermons/src/index.js
+++
@@ -1,9 +0,0 @@
-const {send} = require('micro')
-const middleware = require('./middleware')
-const parseRssFeed = require('./parse-rss-feed')
-const transform = require('./transform')
-
-module.exports = middleware(async (req, res) => {
-  const data = await parseRssFeed()
-  send(res, 200, transform(data))
-})
