e2c7fc38d0b7e28db082c0e519ec8633b19479a9
Brent Mealhouse <bmealhouse@gmail.com>
2020-02-17 08:12:25 -0600

    Update sermons

--- www/components/latest-sermon.js
+++ www/components/latest-sermon.js
@@ -23,9 +23,11 @@ export default function LatestSermon({latestSermon}) {
       <a href={latestSermonHref} css={titleLink}>
         <h3 css={title}>{latestSermon.title}</h3>
       </a>
-      <p css={description}>
-        {latestSermon.description.replace(/<\/?(p|br)>/g, '')}
-      </p>
+      {latestSermon.description && (
+        <p css={description}>
+          {latestSermon.description.replace(/<\/?(p|br)>/g, '')}
+        </p>
+      )}
       <a className="btn btn-default btn-lg" href={latestSermonHref} css={btn}>
         <i
           className="fa fa-play-circle-o fa-lg"
